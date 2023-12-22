d12a3c057327b530d87845e9a4bc3fb80f8006ba
Rezwan66 <shaikhrezwan66@gmail.com>
2023-12-22 11:56:12 +0600

    replaced localhost server link with vercel live link, and deployed to firebase

--- src/hooks/useAxiosPublic.js
+++ src/hooks/useAxiosPublic.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://task-master-server-liard.vercel.app',
 });
 
 const useAxiosPublic = () => {
